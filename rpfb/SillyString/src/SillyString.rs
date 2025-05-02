fde03b37a651e5d7c257677c7bc29fa5cfbc97b4
Richard A. Forth <richard.forth@gmail.com>
2025-05-02 17:27:43 +0000

    Rustling with simple strings

---
+++ rpfb/SillyString/src/SillyString.rs
@@ -0,0 +1,6 @@
+fn main() {
+ let firstname:&str = "Richard";
+ let middle_initial:&str = "A";
+ let surname: &str = "Forth";
+ println!("My name is {} {}. {}", firstname, middle_initial, surname);
+}
