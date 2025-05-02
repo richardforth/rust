c189c63b24f35aa705f295045e1e57072b7e0f3f
Richard Forth <richard.forth@gmail.com>
2025-05-02 13:06:02 +0100

    Checking out \n and \t escape sequences in print/println macros

---
+++ rpfb/EscapeSequences/src/EscapeSequences.rs
@@ -0,0 +1,4 @@
+fn main() {
+ println!("This should be printed on the first line.\nAnd this on the next one!");
+ print!("\nLeaving another line.\tThis is what a tab-space indent looks like.\nHappy Programming!\n");
+}
