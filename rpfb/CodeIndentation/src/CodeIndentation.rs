2668d59339169e69682a0244b9152bff1e4a418f
Richard A. Forth <richard.forth@gmail.com>
2025-05-02 17:55:40 +0000

    Fix an indentation lie!

--- rpfb/CodeIndentation/src/CodeIndentation.rs
+++ rpfb/CodeIndentation/src/CodeIndentation.rs
@@ -10,7 +10,7 @@ println!("Neither is this.");
      println!("This is a line of code that is indented by five characters for no reason.");
       println!("This is a line of code that is indented by six characters for no reason.");
        println!("This is a line of code that is indented by seven characters for no reason.");
-       println!("This is a line of code that is indented by eight characters for no reason.");
+        println!("This is a line of code that is indented by eight characters for no reason.");
 {
 print!("This is a new block that is not indented.");
 println!(" At all!");
