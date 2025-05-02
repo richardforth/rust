1e27806c908c18944803978cec84423537dc24c3
Richard A. Forth <richard.forth@gmail.com>
2025-05-02 17:54:04 +0000

    Demonstrating that there are no enforced style guides by the compiler

---
+++ rpfb/CodeIndentation/src/CodeIndentation.rs
@@ -0,0 +1,21 @@
+fn main() {
+// A bit like C or C++, Rust doesnt enforce any style guides on how its
+// code should be indented, so you are free to style your code as you see fit.
+println!("This is a line of code that is not indented.");
+println!("Neither is this.");
+ println!("This is a line of code that is indented by one character for no reason.");
+  println!("This is a line of code that is indented by two characters for no reason.");
+   println!("This is a line of code that is indented by three characters for no reason.");
+    println!("This is a line of code that is indented by four characters for no reason.");
+     println!("This is a line of code that is indented by five characters for no reason.");
+      println!("This is a line of code that is indented by six characters for no reason.");
+       println!("This is a line of code that is indented by seven characters for no reason.");
+       println!("This is a line of code that is indented by eight characters for no reason.");
+{
+print!("This is a new block that is not indented.");
+println!(" At all!");
+print!("Nobody cares!"); 
+}
+
+}
+
