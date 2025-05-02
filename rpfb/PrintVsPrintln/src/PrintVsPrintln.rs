31e8473cbaaf27d7f5645459855c5935d8fee8f8
Richard Forth <richard.forth@gmail.com>
2025-05-02 11:47:45 +0100

    print!() vs println!() macros.

---
+++ rpfb/PrintVsPrintln/src/PrintVsPrintln.rs
@@ -0,0 +1,10 @@
+fn main() {
+ print!("The print macro. "); // the space at the end was intentional! This is also an inline comment.
+ print!("Another print macro. ");
+ println!("println adds a carriage return at the end, whereas print does not.");
+ println!("Another println macro.");
+ println!("Hello, ");
+ println!("World!");
+ print!("Hello, ");
+ print!("World!");
+}
