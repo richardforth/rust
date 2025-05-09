0635f53b08e40e8ca4f9945af44f168274505b5c
Richard A. Forth <richard.forth@gmail.com>
2025-05-09 17:01:23 +0000

    Type Casting Lab

---
+++ rpfb/TypeCasting/src/DivInt.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let a: isize = 24;
+    let b: isize = 5;
+
+    let q: f64 = (a as f64) / (b as f64);
+
+    println!("\na = {}\nb = {}\nq - {}", a, b, q);
+}
---
+++ rpfb/TypeCasting/src/DivIntFail.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let a: isize = 24;
+    let b: isize = 5;
+
+    let q = a / b;
+
+    println!("\na = {}\nb = {}\nq = {}", a, b, q);
+}
