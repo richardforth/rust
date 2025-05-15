0227b56ad09e911a69f7c25d6bba1e687c1abe4b
Richard Forth <richard.forth@gmail.com>
2025-05-15 12:34:56 +0100

    After successful initial compilation, finished the remainder of the program.

--- rpfb/arrays/src/arrays2.rs
+++ rpfb/arrays/src/arrays2.rs
@@ -15,4 +15,13 @@ fn main() {
     ];
 
     println!("\nElement at index 0: {}", num_array[0]);
+    println!("\nElement at index 1: {}", num_array[1]);
+    println!("\nElement at index 2: {}", num_array[2]);
+    println!("\nElement at index 3: {}", num_array[3]);
+    println!("\nElement at index 4: {}", num_array[4]);
+    println!("\nElement at index 5: {}", num_array[5]);
+    println!("\nElement at index 6: {}", num_array[6]);
+    println!("\nElement at index 7: {}", num_array[7]);
+    println!("\nElement at index 8: {}", num_array[8]);
+    println!("\nElement at index 9: {}", num_array[9]);
 }
