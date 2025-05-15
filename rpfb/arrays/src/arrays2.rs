85711a83941740732c7ae35f216ea7957de62c6e
Richard Forth <richard.forth@gmail.com>
2025-05-15 12:32:15 +0100

    Experiment with pretty-printing arrays in code to see if it compiles, it does.

---
+++ rpfb/arrays/src/arrays2.rs
@@ -0,0 +1,18 @@
+fn main() {
+    // thought I'd try and see if pretty-printing
+    // the array actually compiles, be nice if it does 
+    let num_array: [i32;10] = [
+	15,
+	-76,
+	-23,
+	26,
+	82,
+	-54,
+	34,
+	0,
+	64,
+	96
+    ];
+
+    println!("\nElement at index 0: {}", num_array[0]);
+}
