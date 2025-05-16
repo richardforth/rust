ec7e9f7be56e8663985d4db403b611918475ce13
Richard A. Forth <richard.forth@gmail.com>
2025-05-16 22:15:17 +0000

    First rustle with Vectors.

---
+++ rpfb/vectors/src/vector1.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let num_vector = vec![-5, 8, 0, -2, 9];
+    let fl_vector = vec![4.6, 0.5, 7.3];
+    let name_vector = vec!["Dexter", "Jerry", "Courage"];
+
+    println!("\nnum_vector: {:?}", num_vector);
+    println!("\nnum_vector Size: {}", num_vector.len());
+    println!("\nfl_vector: {:?}", fl_vector);
+    println!("\nfl_vector Size: {}", fl_vector.len());
+    println!("\nname_vector: {:?}", name_vector);
+    println!("\nname_vector Size: {}", name_vector.len());
+}
