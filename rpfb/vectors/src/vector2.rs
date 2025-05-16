97e3cae70d5c184e8cd94c33d0c0b9ad11fed07e
Richard A. Forth <richard.forth@gmail.com>
2025-05-16 22:20:51 +0000

    Simple println! no loops or anything, just accessing elements in a vector

---
+++ rpfb/vectors/src/vector2.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let num_vector = vec![34, -65, 77, 12, -23, 85];
+
+    println!("Element at index 0: {}", num_vector[0]);
+    println!("Element at index 1: {}", num_vector[1]);
+    println!("Element at index 2: {}", num_vector[2]);
+    println!("Element at index 3: {}", num_vector[3]);
+    println!("Element at index 4: {}", num_vector[4]);
+    println!("Element at index 5: {}", num_vector[5]);
+}
