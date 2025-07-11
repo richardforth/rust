1bbdd7e40b5425d28fe1275268d65d6c92571aaf
Richard Forth <richard.forth@gmail.com>
2025-07-11 18:47:50 +0100

    Update garden.rs
    Additional commentary on modules and submodules declarations and where the code is actually found by rust

--- trpl/backyard/src/garden.rs
+++ trpl/backyard/src/garden.rs
@@ -1,2 +1,20 @@
-// Pull garden/vegetables.rs into scope
+// Pull src/garden/vegetables.rs into scope
 pub mod vegetables;
+
+
+/*
+
+Note when we declare a module, rust will look for it in the following places:
+
+1. Inline within curly brackets that replace the semicoloon following mod garden
+2. In the file src/garden.rs
+3. In the file src/garden/mod.rs 
+
+In this particular case we are declaring a sub-module, vegetables, within the garden module, 
+so rust will look for it in the following places:
+
+1. Inline within curly brackets that replace the semicoloon following mod vegetables
+2. In the file src/garden/vegetables.rs
+3. In the file src/garden/vegetables/mod.rs 
+
+*/
