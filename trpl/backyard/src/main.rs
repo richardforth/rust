d8cd26b0eb8e34ba1812779fc320018161949392
Richard Forth <richard.forth@gmail.com>
2025-07-11 18:48:27 +0100

    Update main.rs
    Fix a small typo in commentary

--- trpl/backyard/src/main.rs
+++ trpl/backyard/src/main.rs
@@ -8,7 +8,7 @@ Note when we declare a module, rust will look for it in the following places:
 
 1. Inline within curly brackets that replace the semicoloon following mod garden
 2. In the file src/garden.rs
-3/ In the file src/garden/mod.rs 
+3. In the file src/garden/mod.rs 
 
 */
 
