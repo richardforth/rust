e50dd80e3f81afc74cf7df910d92df60c4229dff
Richard A. Forth <richard.forth@gmail.com>
2025-07-18 19:04:08 +0000

    IMplemeting custom error tpes in real world functions

---
+++ kodekloud/error_handling/implementing_custom_error_types_in_real_functions/src/main.rs
@@ -0,0 +1,39 @@
+// Implementing the Error and Display Traits
+use std::fmt;
+
+#[derive(Debug)]
+enum CustomError {
+    NotFound,
+    PermissionDenied,
+    ConnectionFailed,
+}
+
+impl fmt::Display for CustomError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            CustomError::NotFound => write!(f, "Resource not found"),
+            CustomError::PermissionDenied => write!(f, "Permission denied"),
+            CustomError::ConnectionFailed => write!(f, "Connection failed"),
+        }
+    }
+}
+
+impl std::error::Error for CustomError {}
+
+fn find_user(user_id: u32) -> Result<String, CustomError> {
+    if user_id == 0 {
+        return Err(CustomError::NotFound);
+    } else if user_id == 1 {
+        return Err(CustomError::PermissionDenied);
+    } else if user_id == 2 {
+        return Err(CustomError::ConnectionFailed); 
+    }
+    Ok(String::from("User found"))
+}
+
+fn main() {
+    match find_user(1) {
+        Ok(user) =>  println!("Success: {}", user),
+        Err(e) => println!("Erroг: {}", e),
+    }
+}
