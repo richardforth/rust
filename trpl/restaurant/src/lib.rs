f7f4baa85a3bcb6eebf7c3613fb26cfefbbcfa96
Richard A. Forth <richard.forth@gmail.com>
2025-08-09 13:07:14 +0000

    Added pub Breakfast struct to BOH Mod, with assoc. impl. NOTE: COMPILES WITH WARNINGS

--- trpl/restaurant/src/lib.rs
+++ trpl/restaurant/src/lib.rs
@@ -15,6 +15,20 @@ mod front_of_house {
 }
 
 mod back_of_house {
+   pub struct Breakfast {
+      pub toast: String,
+      seasonal_fruit: String,
+   }
+   
+   impl Breakfast {
+      pub fn summer(toast: &str) -> Breakfast {
+         Breakfast {
+            toast: String::from(toast),
+            seasonal_fruit: String::from("peaches"),
+         }
+      }
+   }
+   
    fn fix_incorrect_order() {
       cook_order();
       super::deliver_order();
