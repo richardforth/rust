a8cb94f8354da718ef511dd58cb44b0b26148be7
Richard A. Forth <richard.forth@gmail.com>
2025-08-09 17:21:01 +0000

    Replace main() and add slog use statements. NOTE: DOES NOT RUN

---
+++ kodekloud/logging/logging_with_slog_crate_for_structured_logging/src/main.rs
@@ -0,0 +1,13 @@
+use slog::{info, o, Drain, Logger};
+use slog_async;
+use slog_term;
+
+fn main() {
+    let decorator = slog_term:: TermDecorator::new().build();
+    let drain slog_term:: FullFormat::new(decorator).build().fuse();
+    let drain slog_async:: Async::new(drain).build().fuse();
+    let log Logger::root (drain, o!());
+
+    info! (log, "Application started"; "version" "1.0.0");
+    info! (log, "This is an info message"; "user" "JohnDoe");
+}
