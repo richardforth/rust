83ecab0bf8a82faa790ec3ab5404f79f762a0c9f
Richard A. Forth <richard.forth@gmail.com>
2025-08-09 17:31:02 +0000

    Fix small bugs in syntax. NOTE: BUILD SUCCESSFUL

--- kodekloud/logging/logging_with_slog_crate_for_structured_logging/src/main.rs
+++ kodekloud/logging/logging_with_slog_crate_for_structured_logging/src/main.rs
@@ -3,11 +3,11 @@ use slog_async;
 use slog_term;
 
 fn main() {
-    let decorator = slog_term:: TermDecorator::new().build();
-    let drain slog_term:: FullFormat::new(decorator).build().fuse();
-    let drain slog_async:: Async::new(drain).build().fuse();
-    let log Logger::root (drain, o!());
+    let decorator = slog_term::TermDecorator::new().build();
+    let drain = slog_term::FullFormat::new(decorator).build().fuse();
+    let drain = slog_async::Async::new(drain).build().fuse();
+    let log = Logger::root (drain, o!());
 
-    info! (log, "Application started"; "version" "1.0.0");
-    info! (log, "This is an info message"; "user" "JohnDoe");
+    info!(log, "Application started"; "version" => "1.0.0");
+    info!(log, "This is an info message"; "user" => "JohnDoe");
 }
