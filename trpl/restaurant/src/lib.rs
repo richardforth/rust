8cda56b77c38d4c7af787bb6424c4dd2ab80e6c0
Richard A. Forth <richard.forth@gmail.com>
2025-08-09 12:09:43 +0000

    Added relative path NOTE: DOES NOT COMPILE

--- trpl/restaurant/src/lib.rs
+++ trpl/restaurant/src/lib.rs
@@ -17,4 +17,7 @@ mod front_of_house {
 pub fn eat_at_restaurant() {
     // Absolute path
     crate::front_of_house::hosting::add_to_waitlist();
+ 
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
 }
