c000a89ca1c87222fe137858f21a3f121766d9c3
Richard A. Forth <richard.forth@gmail.com>
2025-08-09 13:11:40 +0000

    Updated eat_at_restaurant pub fn. NOTE: COMPILES WITH WARNINGS

--- trpl/restaurant/src/lib.rs
+++ trpl/restaurant/src/lib.rs
@@ -1,6 +1,6 @@
 mod front_of_house {
-   pub mod hosting {
-      pub fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
 
         fn seat_at_table() {}
     }
@@ -15,34 +15,39 @@ mod front_of_house {
 }
 
 mod back_of_house {
-   pub struct Breakfast {
-      pub toast: String,
-      seasonal_fruit: String,
-   }
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
    
-   impl Breakfast {
-      pub fn summer(toast: &str) -> Breakfast {
-         Breakfast {
-            toast: String::from(toast),
-            seasonal_fruit: String::from("peaches"),
-         }
-      }
-   }
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+           }
+        }
+    }
    
-   fn fix_incorrect_order() {
-      cook_order();
-      super::deliver_order();
-   }
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
 
-   fn cook_order() {}
+    fn cook_order() {}
 }
 
 pub fn eat_at_restaurant() {
-    // Absolute path (Recommended by 'The Book')
-    crate::front_of_house::hosting::add_to_waitlist();
- 
-    // Relative path
-    front_of_house::hosting::add_to_waitlist();
+    // Order a breakfast in the summer with Rye toast.
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like.
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+    // The next line won't compile if we uncomment it; we're not
+    // allowed to see or modify the seasonal fruit that comes
+    // with the meal.
+    // meal.seasonal_fruit = String::from("blueberries");
 }
 
 fn deliver_order() {}
