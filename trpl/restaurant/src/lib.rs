246e45b627f60442da76d490b7f78a1b997de1e8
Richard A. Forth <richard.forth@gmail.com>
2025-08-09 13:27:52 +0000

    Move eat_at_restaurant function within new customer module. NOTE: DOES NOT COMPILE

--- trpl/restaurant/src/lib.rs
+++ trpl/restaurant/src/lib.rs
@@ -44,21 +44,26 @@ mod back_of_house {
 
 use crate::front_of_house::hosting;
 
-pub fn eat_at_restaurant() {
-    hosting::add_to_waitlist;
-    // Order a breakfast in the summer with Rye toast.
-    let mut meal = back_of_house::Breakfast::summer("Rye");
-    // Change our mind about what bread we'd like.
-    meal.toast = String::from("Wheat");
-    println!("I'd like {} toast please", meal.toast);
+mod customer {
+    pub fn eat_at_restaurant() {
+        hosting::add_to_waitlist;
+        // Order a breakfast in the summer with Rye toast.
+        let mut meal = back_of_house::Breakfast::summer("Rye");
+        // Change our mind about what bread we'd like.
+        meal.toast = String::from("Wheat");
+        println!("I'd like {} toast please", meal.toast);
 
-    // The next line won't compile if we uncomment it; we're not
-    // allowed to see or modify the seasonal fruit that comes
-    // with the meal.
-    // meal.seasonal_fruit = String::from("blueberries");
+        // The next line won't compile if we uncomment it; we're not
+        // allowed to see or modify the seasonal fruit that comes
+        // with the meal.
+        // meal.seasonal_fruit = String::from("blueberries");
 
-    let order1 = back_of_house::Appetizer::Soup;
-    let order2 = back_of_house::Appetizer::Salad;
+        let order1 = back_of_house::Appetizer::Soup;
+        let order2 = back_of_house::Appetizer::Salad;
+        pub fn eat_at_restaurant() {
+            hosting::add_to::waitlist();
+        }
+    }
 }
 
 fn deliver_order() {}
