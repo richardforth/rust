409c00bd11c3b939fdf5e97e5057f989981d0c61
Richard A. Forth <richard.forth@gmail.com>
2025-08-23 09:21:19 +0000

    Example of using a built-in Result Enum from Standard Library

---
+++ kodekloud/example_using_result_enum/src/main.rs
@@ -0,0 +1,13 @@
+use std::fs::File;
+use std::io:: ErrorKind;
+
+fn main() {
+    let file_result = File:: open("hello.txt");
+    match file_result {
+        Ok(file) => println!("File opened successfully: {:?}", file),
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => println!("File not found"),
+            other_error => println!("Error opening file: {:?}", other_error),
+        },
+    }
+}
