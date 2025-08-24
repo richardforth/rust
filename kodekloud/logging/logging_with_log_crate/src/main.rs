3673f021905960d59b643ae31d5c4a2c4606e8c9
Richard A. Forth <richard.forth@gmail.com>
2025-08-24 10:42:34 +0000

    Add comment to sho how to set log level using 'RUST_LOG=debug cargo run'

--- kodekloud/logging/logging_with_log_crate/src/main.rs
+++ kodekloud/logging/logging_with_log_crate/src/main.rs
@@ -2,6 +2,7 @@
 use log::{debug, error, info, warn};
 
 fn main() {
+    // RUST_LOG=debug cargo run to see more than just error!() logs to the console
     env_logger::init();
 
     info!("This is an info message");
