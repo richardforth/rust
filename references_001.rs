4d800c035c28db0bd8d00101f89703df7f46d175
Richard A. Forth <richard.forth@gmail.com>
2025-09-06 13:27:03 +0000

    Fix so it compiles by creating a separate scope for r1 and its println macro

--- references_001.rs
+++ references_001.rs
@@ -1,13 +1,19 @@
 fn main() {
     let mut s = String::from("Rust");
-    let r1 = &s;
+    {
+        let r1 = &s;
+        println!("{}", r1);
+    }
     let r2 = &mut s;
-    println!("{}", r1);
     r2.push_str(" language"); 
+    println!("{}", r2);
 }
 
 /*
 
-Firt commit: Original Book Code asking to fix code that will not compile
+First commit: Original Book Code asking to fix code that will not compile
+Second Commit: Fix so it compiles: 
+                 - Put r1 and its println macro in its own scope,
+                 -  add a println macro for r2
 
 */
