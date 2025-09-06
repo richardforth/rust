cd0e4fa8a9d73236542a9f0ea668b2a8ba6f33f3
Richard A. Forth <richard.forth@gmail.com>
2025-09-06 13:30:24 +0000

    Third commit: Alternative fix without a scope, print directly from &s before mutable borrow

--- references_001.rs
+++ references_001.rs
@@ -1,9 +1,6 @@
 fn main() {
     let mut s = String::from("Rust");
-    {
-        let r1 = &s;
-        println!("{}", r1);
-    }
+    println!("{}", &s);
     let r2 = &mut s;
     r2.push_str(" language"); 
     println!("{}", r2);
@@ -15,5 +12,6 @@ First commit: Original Book Code asking to fix code that will not compile
 Second Commit: Fix so it compiles: 
                  - Put r1 and its println macro in its own scope,
                  -  add a println macro for r2
-
+Third commit: Alternative fix without a scope, print directly from &s
+                before immutable borrow
 */
