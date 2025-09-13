e645002dfb0248f6e562784bffe214775bf6a9c6
Richard A. Forth <richard.forth@gmail.com>
2025-09-13 13:54:19 +0000

    Add a greeter library with a test that passes.

---
+++ trpl/greet_name/src/lib.rs
@@ -0,0 +1,14 @@
+pub fn greet_name(name: &str) -> String {
+     format!("Hello {name}!")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn greeting_contains_name() {
+        let greeting = greet_name("Richard");
+        assert!(greeting.contains("Richard"));
+    }
+}
