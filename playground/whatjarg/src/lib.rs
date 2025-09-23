0732a2fd304c8726892ef42392b18750d22d9c85
Richard A. Forth <richard.forth@gmail.com>
2025-09-23 23:08:48 +0000

    Added ABC expansion

--- playground/whatjarg/src/lib.rs
+++ playground/whatjarg/src/lib.rs
@@ -1,8 +1,11 @@
 pub fn get_jarg(sc: &String) -> String {
      let normalized_sc = sc.to_lowercase();
       match normalized_sc.as_str() {
+          "abc" => {
+              format!("\n\tAbstract Base Class (Python).\n\tABC Programming language, precusror to Python.\n")
+          }
           "soflam" => {
-              format!("Special Operations Forces Laser Aquisition Marker\n")
+              format!("\n\tSpecial Operations Forces Laser Aquisition Marker\n")
           }
           _ => {
               format!("\nShortcode {} not recognised.", normalized_sc.as_str())
