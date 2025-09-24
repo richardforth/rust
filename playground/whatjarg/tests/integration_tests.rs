c9d135196b9bc3405702df8f4e766987b3e88ffb
Richard Forth <richard.forth@gmail.com>
2025-09-24 09:18:07 +0100

    re-ordered tests

--- playground/whatjarg/tests/integration_tests.rs
+++ playground/whatjarg/tests/integration_tests.rs
@@ -1,28 +1,6 @@
 use whatjarg;
 
 #[test]
-fn soflam_expands_correctly_caps() {
-     let test_string = String::from("SOFLAM");
-     let expected_text = String::from("Aquisition Marker");
-     let expansion = whatjarg::get_jarg(&test_string);
-        assert!(
-            expansion.contains(&expected_text),
-            "expansion not contain expected text: `{expected_text}`  value was `{expansion}`"
-        );
-}
-
-#[test]
-fn soflam_expands_correctly_lower() {
-     let test_string = String::from("soflam");
-     let expected_text = String::from("Aquisition Marker");
-     let expansion = whatjarg::get_jarg(&test_string);
-        assert!(
-            expansion.contains(&expected_text),
-            "expansion not contain expected text: `{expected_text}`  value was `{expansion}`"
-        );
-}
-
-#[test]
 fn roi_expands_correctly_caps() {
      let test_string = String::from("ROI");
      let expected_text = String::from("Investment");
@@ -37,6 +15,29 @@ fn roi_expands_correctly_caps() {
 fn roi_expands_correctly_lower() {
      let test_string = String::from("roi");
      let expected_text = String::from("Investment");
+     let expansion = whatjarg::get_jarg(&test_string);
+        assert!(
+            expansion.contains(&expected_text),
+            "expansion not contain expected text: `{expected_text}`  value was `{expansion}`"
+        );
+}
+
+
+#[test]
+fn soflam_expands_correctly_caps() {
+     let test_string = String::from("SOFLAM");
+     let expected_text = String::from("Aquisition Marker");
+     let expansion = whatjarg::get_jarg(&test_string);
+        assert!(
+            expansion.contains(&expected_text),
+            "expansion not contain expected text: `{expected_text}`  value was `{expansion}`"
+        );
+}
+
+#[test]
+fn soflam_expands_correctly_lower() {
+     let test_string = String::from("soflam");
+     let expected_text = String::from("Aquisition Marker");
      let expansion = whatjarg::get_jarg(&test_string);
         assert!(
             expansion.contains(&expected_text),
